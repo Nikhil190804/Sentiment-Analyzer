c2d05f4c17ca86c6932d6d790cef681d3064e494
Nikhil <nikhil22322@iiitd.ac.in>
2025-06-05 22:45:12 +0530

    added yt api setup

--- Backend/server.py
+++ Backend/server.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import re
-
+from dotenv import load_dotenv
+import os
+import requests
 app = Flask(__name__)
 CORS(app)
 
+load_dotenv()
 
 def extract_video_id_from_url(url):
     match = re.search(r"(?:v=|\/|youtu\.be\/|embed\/|shorts\/)([a-zA-Z0-9_-]{11})", url)
@@ -32,6 +35,25 @@ def query():
     video_id = extract_video_id_from_url(video_url)
     if(video_id==None):
         return jsonify({"error": "Invalid URL"}), 400
+    
+    ALL_COMMENTS = []
+
+    API_KEY = os.getenv("YOUTUBE_API")
+    BASE_URL = os.getenv("BASE_URL")
+    params = {
+        "part": "snippet",
+        "videoId": video_id,
+        "key": API_KEY,
+        "maxResults": 100,
+        "textFormat": "plainText"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+    print(len(data))
+    print(data.keys())
+    print((data["items"][0]))
+    
     return jsonify({"message": f"This is a sample API response for query! {video_id}"})
 
 if __name__ == '__main__':
